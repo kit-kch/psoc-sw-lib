11f6a380c407ecb43738fe50a96d2f9180bdae3b
Johannes Pfau <johannes.pfau@kit.edu>
2025-08-06 14:45:07 +0200

    Introduce new PSOC UART banner

--- source/psoc_board.c
+++ source/psoc_board.c
@@ -12,13 +12,36 @@ bool psoc_board_setup(bool force)
     {
         // Need to initialize IO Mux on PSOC Chip
         IOMUX_REG_FUNC = FUNC_UART0;
-        neorv32_uart0_printf("Running on PSOC SOC:\n");
+
+        neorv32_uart0_printf("\n\n\n");
+        neorv32_uart0_printf("#===========================================================================#\n");
+        neorv32_uart0_printf("                                                                             \n");
+        neorv32_uart0_printf("PPPPPPPPPPPPPPPPP      SSSSSSSSSSSSSSS                          CCCCCCCCCCCCC\n");
+        neorv32_uart0_printf("P::::::::::::::::P   SS:::::::::::::::S                      CCC::::::::::::C\n");
+        neorv32_uart0_printf("P::::::PPPPPP:::::P S:::::SSSSSS::::::S                    CC:::::::::::::::C\n");
+        neorv32_uart0_printf("PP:::::P     P:::::PS:::::S     SSSSSSS                   C:::::CCCCCCCC::::C\n");
+        neorv32_uart0_printf("  P::::P     P:::::PS:::::S               ooooooooooo    C:::::C       CCCCCC\n");
+        neorv32_uart0_printf("  P::::P     P:::::PS:::::S             oo:::::::::::oo C:::::C              \n");
+        neorv32_uart0_printf("  P::::PPPPPP:::::P  S::::SSSS         o:::::::::::::::oC:::::C              \n");
+        neorv32_uart0_printf("  P:::::::::::::PP    SS::::::SSSSS    o:::::ooooo:::::oC:::::C              \n");
+        neorv32_uart0_printf("  P::::PPPPPPPPP        SSS::::::::SS  o::::o     o::::oC:::::C              \n");
+        neorv32_uart0_printf("  P::::P                   SSSSSS::::S o::::o     o::::oC:::::C              \n");
+        neorv32_uart0_printf("  P::::P                        S:::::So::::o     o::::oC:::::C              \n");
+        neorv32_uart0_printf("  P::::P                        S:::::So::::o     o::::o C:::::C       CCCCCC\n");
+        neorv32_uart0_printf("PP::::::PP          SSSSSSS     S:::::So:::::ooooo:::::o  C:::::CCCCCCCC::::C\n");
+        neorv32_uart0_printf("P::::::::P          S::::::SSSSSS:::::So:::::::::::::::o   CC:::::::::::::::C\n");
+        neorv32_uart0_printf("P::::::::P          S:::::::::::::::SS  oo:::::::::::oo      CCC::::::::::::C\n");
+        neorv32_uart0_printf("PPPPPPPPPP           SSSSSSSSSSSSSSS      ooooooooooo           CCCCCCCCCCCCC\n");
+        neorv32_uart0_printf("                                                                             \n");
+        neorv32_uart0_printf("ASIC LAB                                                         kch.dev/psoc\n");
+        neorv32_uart0_printf("#===========================================================================#\n");
+        neorv32_uart0_printf("=> Running on PSOC SOC:\n");
     }
     else
     {
         if (force)
         {
-            neorv32_uart0_printf("Not running on PSOC SOC. Stopping execution\n");
+            neorv32_uart0_printf("=> Not running on PSOC SOC. Stopping execution\n");
             while (true) {}
         }
         else
@@ -38,18 +61,18 @@ bool psoc_board_setup(bool force)
                 break;
             default:
         }
-        neorv32_uart0_printf("    ASIC Revision %d\n    %s\n", rev, name);
+        neorv32_uart0_printf("    * ASIC Revision %d\n    * %s\n", rev, name);
     }
     else
     {
-        neorv32_uart0_printf("    FPGA Emulation\n");
+        neorv32_uart0_printf("    * FPGA Emulation\n");
     }
 
     // Not using SPI and PWM, use those as GPIOs
-    neorv32_uart0_printf("Setting up IO multiplexer\n");
+    neorv32_uart0_printf("=> Setting up IO multiplexer\n");
     IOMUX_REG_FUNC = (FUNC_I2S | FUNC_DAC | FUNC_I2C | FUNC_UART0 | FUNC_SD);
 
-    neorv32_uart0_printf("Setting up GPIOs\n");
+    neorv32_uart0_printf("=> Setting up GPIOs\n");
     IOMUX_REG_DIR = ((DIR_OUTPUT << PSOC_LED2) | (DIR_OUTPUT << PSOC_LED3));
 
     neorv32_gpio_pin_set(PSOC_LED2, 1);
